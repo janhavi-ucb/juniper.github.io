9c88b34262c6ec52a1dae2ec7f1e066a0cba2b07
janhavi-ucb <109857272+janhavi-ucb@users.noreply.github.com>
2024-03-28 08:25:27 +0530

    removed actual website code

--- input_to_diagnosis.py
+++
@@ -1,9 +0,0 @@
-class Diagnosis():
-    def __init__(self,treated=None,original=None):
-        self.treated_prompt = treated
-        self.original_prompt = original
-    def set_prompt(self,treated,original):
-        self.treated_prompt = treated
-        self.original_prompt = original
-    def get_treated_prompt(self):
-        return self.treated_prompt, self.original_prompt
